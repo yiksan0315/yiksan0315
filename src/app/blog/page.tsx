609754cc33cbd75f20337da95f373d9c81829443
yiksan0315 <1889sanik1889@gmail.com>
2024-08-29 15:56:25 +0900

    temp

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ export default async function Page() {
   let posts = await data.json();
   return (
     <ul>
-      {posts.map((post) => (
+      {posts.map((post: any) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ul>
